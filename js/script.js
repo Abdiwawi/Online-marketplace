e08e622a7776ed201a86f93a3e569733eca168a8
Abdiwawi <abdijamzilan@gmail.com>
2017-04-25 20:46:39 +0300

    add third variable

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $(#"blanks form").submit(function(event){ /*referncing of form*/
     var Firstname = $("input#Firstname") .val();
     var Surname = $("input#Surname") .val();
+    var Residence = $("input#Residence") .val();
+    
 
   }
 

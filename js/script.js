b8a2db033c2110e076837924554d5d35f7235439
Abdiwawi <abdijamzilan@gmail.com>
2017-04-25 20:45:20 +0300

    add second variable

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(#"blanks form").submit(function(event){ /*referncing of form*/
-    var Firstname =$("input#Firstname") .val();
+    var Firstname = $("input#Firstname") .val();
+    var Surname = $("input#Surname") .val();
 
   }
 

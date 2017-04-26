3db654fef2640282b42a06c7ad34924d72fd5d73
Abdiwawi <abdijamzilan@gmail.com>
2017-04-26 21:22:24 +0300

    add code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event){ /*referncing of form*/
-    var Firstname = $("input#Firstname") .val();
-    var Surname = $("input#Surname") .val();
-    var Residence = $("input#Residence") .val();
-    var StreetAddress = $("input#StreetAddress") .val();
-    var HouseNumber = $("input#HouseNumber") .val();
+    $("#blanks form").submit(function(event){ /*referncing of form*/
+      var Firstname = $("input#Firstname") .val();
+      var Surname = $("input#Surname") .val();
+      var Residence = $("input#Residence") .val();
+      var StreetAddress = $("input#StreetAddress") .val();
+      var HouseNumber = $("input#HouseNumber") .val();
 
-    $(".Firstname").append(FirstnameInput);
-    $(".Surname").append(SurnameInput);
-    $(".Residence").append(ResidenceInput);
-    $(".StreetAddress").append(StreetAddressInput);
-    $(".HouseNumber").append(HouseNumberInput);
-}
+      $(".Firstname").append(Firstname);
+      $(".Surname").append(Surname);
+      $(".Residence").append(Residence);
+      $(".StreetAddress").append(StreetAddress);
+      $(".HouseNumber").append(HouseNumber);
+
+      $("#story").show();
+      $(".Receipt").show();
+
+      event.preventDefault();
+    });
+  });

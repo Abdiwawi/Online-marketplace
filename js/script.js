4d2237e3cd1c5e9e7038b087b8eb095b0355bebc
Abdiwawi <abdijamzilan@gmail.com>
2017-04-26 20:36:32 +0300

    add code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  $(#"blanks form").submit(function(event){ /*referncing of form*/
+  $("#blanks form").submit(function(event){ /*referncing of form*/
     var Firstname = $("input#Firstname") .val();
     var Surname = $("input#Surname") .val();
     var Residence = $("input#Residence") .val();
     var StreetAddress = $("input#StreetAddress") .val();
-    var HouseNumber = $("input#Residence") .val();
-    var
+    var HouseNumber = $("input#HouseNumber") .val();
 
-  }
-
-
+    $(".Firstname").append(FirstnameInput);
+         $(".Surname").append(SurnameInput);
+         $(".Residence").append(ResidenceInput);
+         $(".StreetAddress").append(StreetAddressInput);
+         $(".verb").append(verbInput);
+         $(".noun").append(nounInput);
 
 }
